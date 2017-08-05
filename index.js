422a892b2ee9c97c2789cb60af1713e8e134732c
rossillingworth <ross_illingworth@hotmail.com>
2017-08-05 09:24:04 +0100

    adding reading-a-file notes

--- index.js
+++ index.js
@@ -51,16 +51,5 @@ function loadFileAsArray(filePath){
 }
 
 
-function readLargeFile(){
-    var fs = require('fs');
-    var readline = require('readline');
 
-    var filename = process.argv[2];
-    readline.createInterface({
-        input: fs.createReadStream(filename),
-        terminal: false
-    }).on('line', function(line) {
-        console.log('Line: ' + line);
-    });
-}
 
